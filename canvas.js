0f4a26f375ea914b2eb358e53dc86b73c4e3b6a1
Duncan Dean <duncangleeddean@gmail.com>
2013-05-28 15:52:03 +0200

    Added canvas link

--- canvas.js
+++ canvas.js
@@ -1,35 +1,13 @@
+var amount = 20;
+var myPath = new Path();
 
-
-// The amount of segment points we want to create:
-var amount = 100;
-
-// The maximum height of the wave:
-var height = 30;
-
-// Create a new path and style it:
-var path = new Path();
-path.strokeColor = 'black';
-
-// Add 5 segment points to the path spread out
-// over the width of the view:
-for (var i = 0; i <= amount; i++) {
-    path.add(new Point(i / amount, 1) * view.size);
+for (var i = 0; i < amount; i++){
+    myPath.add(new Point(i/amount, 1) * view.size);
 }
 
-
+myPath.strokeColor = 'black';
 
 function onFrame(event) {
-    // Loop through the segments of the path:
-    for (var i = 0; i <= amount; i++) {
-        var segment = path.segments[i];
-
-        // A cylic value between -1 and 1
-        var sinus = Math.sin(event.time * 10 + i);
-        
-        // Change the y position of the segment point:
-        segment.point.y = sinus * height + 100;
-    }
-    // Uncomment the following line and run the script again
-    // to smooth the path:
-    path.smooth();
+    
+    $('#times').html(parseInt(event.time));
 }
